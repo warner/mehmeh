a40b945ef43d771a806a05d5658e5f117cf67b67
Dan Walkowski <dan.walkowski@gmail.com>
2012-11-09 15:16:28 -0800

    Initial Commit

---
+++ SkyCrane/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SkyCrane
+//
+//  Created by Dan Walkowski on 11/9/12.
+//  Copyright (c) 2012 Mozilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SkyCraneTests/SkyCraneTests.m
@@ -0,0 +1,32 @@
+//
+//  SkyCraneTests.m
+//  SkyCraneTests
+//
+//  Created by Dan Walkowski on 11/9/12.
+//  Copyright (c) 2012 Mozilla. All rights reserved.
+//
+
+#import "SkyCraneTests.h"
+
+@implementation SkyCraneTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SkyCraneTests");
+}
+
+@end
