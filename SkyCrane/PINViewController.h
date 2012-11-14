e321be640dd9a62cb69f37cf03ff419c8d124d25
Dan Walkowski <dan.walkowski@gmail.com>
2012-11-13 23:13:44 -0800

    pin code entry works, and keeps track of failed attempts

--- SkyCrane/PINViewController.h
+++ SkyCrane/PINViewController.h
@@ -9,8 +9,19 @@
 #import <UIKit/UIKit.h>
 
 @interface PINViewController : UIViewController
+{
+  int nextDigit;
+  int pinAttempt[4]; //the current attempt at the pin
+  int failedAttempts;
+  int actualPin[4];
+  
+}
 
 @property (strong, nonatomic) NSDictionary* plaintext; //the raw dictionary of stuff from the JSON file
+@property (strong, nonatomic) NSArray* pin; //the correct pin, extracted from the json above
 
+
+
+- (IBAction) numEnter:(id)sender;
 @end
 
--- SkyCrane/PINViewController.m
+++ SkyCrane/PINViewController.m
@@ -17,11 +17,45 @@
 
 @implementation PINViewController
 
+- (IBAction) numEnter:(id) sender;
+{
+  NSString* buttonString = ((UIButton*)sender).titleLabel.text;
+  int value = [buttonString intValue];
+  pinAttempt[nextDigit] = value;
+  nextDigit++;
+  
+  if (nextDigit == 4)  //check for correctness
+  {
+    if ((pinAttempt[0] == actualPin[0]) &&
+        (pinAttempt[1] == actualPin[1]) &&
+        (pinAttempt[2] == actualPin[2]) &&
+        (pinAttempt[3] == actualPin[3]) )
+    {
+      //success!
+      pinAttempt[0] = pinAttempt[1] = pinAttempt[2] = pinAttempt[3] = nextDigit = 0;
+      //initiate the segue
+      NSLog(@"CORRECT!");
+      [self performSegueWithIdentifier: @"Unlock" sender: self];
+
+    }
+    else
+    {
+      //fail!
+      pinAttempt[0] = pinAttempt[1] = pinAttempt[2] = pinAttempt[3] = nextDigit = 0;
+      failedAttempts++;
+      NSLog(@"failed attempts: %i", failedAttempts);
+    }
+  }
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-  // Let's read the contents of the file of passwords!  whee!
+  pinAttempt[0] = pinAttempt[1] = pinAttempt[2] = pinAttempt[3] = nextDigit = 0;
+  failedAttempts = 0;
+
+  // Let's read the file of passwords!  whee!
   NSArray *pathArray = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
   NSString *documentsDirectory = [pathArray objectAtIndex:0];
   NSString *filePath = [documentsDirectory stringByAppendingPathComponent:SITES_FILE];
@@ -33,7 +67,14 @@
     NSLog(@"There was an error: %@", [error description]);
   } else {
     
-   _plaintext  = [self parseJSONdata:fileData];    
+   _plaintext  = [self parseJSONdata:fileData];
+    NSString* pinStr = [_plaintext objectForKey:@"pin"];
+    
+    for (int j=0; j<4; j++)
+    {
+      actualPin[j] = [[pinStr substringWithRange:NSMakeRange(j, 1)] intValue];
+//      NSLog(@"PIN digit %i = %i", j, actualPin[j]);
+    }
   }
 
 }
