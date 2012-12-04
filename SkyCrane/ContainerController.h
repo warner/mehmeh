e0eaa161823ee2cb185330094fd415d59700f5b7
Dan Walkowski <dan.walkowski@gmail.com>
2012-12-04 15:54:26 -0800

    many missing and duplicate files straightened out

---
+++ SkyCrane/ContainerController.h
@@ -0,0 +1,13 @@
+//
+//  ContainerController.h
+//  SkyCrane
+//
+//  Created by Dan Walkowski on 11/12/12.
+//  Copyright (c) 2012 Mozilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContainerController : UINavigationController
+
+@end
---
+++ SkyCrane/ContainerController.m
@@ -0,0 +1,38 @@
+//
+//  ContainerController.m
+//  SkyCrane
+//
+//  Created by Dan Walkowski on 11/12/12.
+//  Copyright (c) 2012 Mozilla. All rights reserved.
+//
+
+#import "ContainerController.h"
+
+@interface ContainerController ()
+
+@end
+
+@implementation ContainerController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
