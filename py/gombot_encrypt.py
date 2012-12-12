ae2e55d5cbebdbe1144d009729385513d04cdb33
Brian Warner <warner@lothar.com>
2012-12-12 14:58:48 -0800

    gombot_encrypt.py: switch to PKCS#5 padding, remove 'datalen' field

--- py/gombot_encrypt.py
+++ py/gombot_encrypt.py
@@ -7,11 +7,13 @@ from gombot_kdf import do as do_kdf
 
 tohex = binascii.hexlify
 
-def padding_needed(datalen):
-    needed = 0
-    while (datalen+needed) % 16 != 0:
-        needed += 1
-    return needed
+# AES-CBC, PKCS#5. SJCL.js can do that.
+
+assert AES.block_size == 16
+
+def pkcs5_padding(datalen):
+    needed = 16-(datalen%16)
+    return chr(needed)*needed
 
 def encrypt(email, password, data):
     authKey, cryptKey = do_kdf(email, password)
@@ -19,15 +21,13 @@ def encrypt(email, password, data):
     hmacKey = PBKDF2(cryptKey, "identity.mozilla.com/gombot/v1/data/HMAC", 1, 32)
     IV = os.urandom(16)
     c = AES.new(aesKey, mode=AES.MODE_CBC, IV=IV)
-    assert AES.block_size == 16
-    padded_data = data + "\x00" * padding_needed(len(data))
-    data_len = struct.pack(">Q", len(data))
-    assert len(data_len) == 8
+    padded_data = data + pkcs5_padding(len(data))
+    assert len(padded_data)%AES.block_size == 0, (len(padded_data), AES.block_size)
     ct = c.encrypt(padded_data)
-    msg = data_len+IV+ct
-    # [DATALEN, IV, enc(PADDEDDATA), mac(DATALEN+IV+enc(PADDEDDATA))]
+    msg = IV+ct
+    # [IV, enc(PADDEDDATA), mac(IV+enc(PADDEDDATA))]
     mac = hmac.new(hmacKey, msg, hashlib.sha256).digest()
-    #print "enc", [tohex(data_len), tohex(IV), tohex(ct), tohex(mac)]
+    #print "enc", [tohex(IV), tohex(ct), tohex(mac)]
     msgmac = msg+mac
     return msgmac
 
@@ -35,17 +35,16 @@ def decrypt(email, password, msgmac):
     authKey, cryptKey = do_kdf(email, password)
     aesKey = PBKDF2(cryptKey, "identity.mozilla.com/gombot/v1/data/AES", 1, 32)
     hmacKey = PBKDF2(cryptKey, "identity.mozilla.com/gombot/v1/data/HMAC", 1, 32)
-    msg = msgmac[:-32]
-    mac = msgmac[-32:] # covers everything else: (DATALEN, IV, enc())
+    msg,mac = msgmac[:-32], msgmac[-32:]
+    # mac covers everything else: (IV+enc(data+padding))
     if mac != hmac.new(hmacKey, msg, hashlib.sha256).digest():
         raise ValueError("Corrupt encrypted data")
-    (datalen,) = struct.unpack(">Q", msg[:8])
-    IV = msg[8:8+16]
-    ct = msg[8+16:]
-    #print "dec", [datalen, tohex(msg[:8]), tohex(IV), tohex(ct), tohex(mac)]
+    IV,ct = msg[:16], msg[16:]
+    #print "dec", [tohex(IV), tohex(ct), tohex(mac)]
     c = AES.new(aesKey, mode=AES.MODE_CBC, IV=IV)
     padded_data = c.decrypt(ct)
-    data = padded_data[:datalen]
+    pad = ord(padded_data[-1])
+    data = padded_data[:-pad]
     return data
 
 msgmac = encrypt(u"foo@example.org", u"password", "data")
