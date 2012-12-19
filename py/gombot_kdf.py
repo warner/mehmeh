4854f71a2e783af343d206d5b8b78df2ba52a957
Brian Warner <warner@lothar.com>
2012-12-18 16:30:15 -0800

    gombot_kdf.py: move everything into this file

--- py/gombot_kdf.py
+++ py/gombot_kdf.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
 
-import hashlib, hmac, binascii
+import os, hashlib, hmac, binascii
+from pbkdf2 import PBKDF2
+from Crypto.Cipher import AES
 
 assert len(hmac.new("", "", hashlib.sha256).digest()) == 256/8 == 32
 tohex = binascii.hexlify
@@ -28,34 +30,93 @@ def HKDF_K(PRK, length_bytes, CTXinfo):
 def HKDF(XTS, SKM, CTXinfo, length_bytes):
     return HKDF_K(HKDF_PRK(XTS, SKM), length_bytes, CTXinfo)
 
-from pbkdf2 import PBKDF2
+def dump(which, s, stride=16):
+    print "%s: %s" % (which, "\n".join([tohex(s)[i:i+stride]
+                                        for i in range(0, len(tohex(s)), stride)
+                                        ]))
 
-def do(email, password):
-    salt = "identity.mozilla.com/gombot/v1/derivation:" + email.encode("utf-8")
-    derivedKey = PBKDF2(password.encode("utf-8"), salt, 250*1000, 256/8)
-    #print "derived", tohex(derivedKey)
-    authKey = PBKDF2(derivedKey,
-                     "identity.mozilla.com/gombot/v1/authentication",
-                     1, 256/8)
-    cryptKey = PBKDF2(derivedKey,
-                     "identity.mozilla.com/gombot/v1/encryption",
-                      1, 256/8)
+version_prefix = "identity.mozilla.com/gombot/v1:"
+def salt(name):
+    return "identity.mozilla.com/gombot/v1/%s" % name
+
+def do_kdf(email, password, secret=""):
+    dump("email", email.encode("utf-8"))
+    dump("password", password.encode("utf-8"))
+    derivedKey = PBKDF2(secret+password.encode("utf-8"),
+                        salt("derivation:" + email.encode("utf-8")),
+                        250*1000, 256/8)
+    dump("master", derivedKey)
+    authKey = PBKDF2(derivedKey, salt("authentication"), 1, 256/8)
+    cryptKey = PBKDF2(derivedKey, salt("encryption"), 1, 256/8)
+    dump(" authKey ", authKey)
+    dump(" cryptKey", cryptKey)
     return authKey, cryptKey
+
+
+
+# AES-CBC, PKCS#5. SJCL.js can do that.
+
+assert AES.block_size == 16
+
+def pkcs5_padding(datalen):
+    needed = 16-(datalen%16)
+    return chr(needed)*needed
+
+def encrypt(email, password, data, secret=""):
+    authKey, cryptKey = do_kdf(email, password, secret)
+    aesKey = PBKDF2(cryptKey, salt("data/AES"), 1, 32)
+    hmacKey = PBKDF2(cryptKey, salt("data/HMAC"), 1, 32)
+    dump("aesKey", aesKey)
+    dump("hmacKey", hmacKey)
+    IV = os.urandom(16)
+    dump("IV", IV)
+    c = AES.new(aesKey, mode=AES.MODE_CBC, IV=IV)
+    padded_data = data + pkcs5_padding(len(data))
+    assert len(padded_data)%AES.block_size == 0, (len(padded_data), AES.block_size)
+    ct = c.encrypt(padded_data)
+    msg = IV+ct
+    # [IV, enc(PADDEDDATA), mac(IV+enc(PADDEDDATA))]
+    mac = hmac.new(hmacKey, msg, hashlib.sha256).digest()
+    print "enc", [tohex(IV), tohex(ct), tohex(mac)]
+    dump("ct", ct)
+    versioned_msgmac = version_prefix+msg+mac
+    dump("versioned_msgmac", versioned_msgmac, stride=32)
+    return versioned_msgmac
+
+def decrypt(email, password, versioned_msgmac, secret=""):
+    assert versioned_msgmac.startswith(version_prefix)
+    msgmac = versioned_msgmac[len(version_prefix):]
+    authKey, cryptKey = do_kdf(email, password, secret)
+    aesKey = PBKDF2(cryptKey, salt("data/AES"), 1, 32)
+    dump("aesKey", aesKey)
+    hmacKey = PBKDF2(cryptKey, salt("data/HMAC"), 1, 32)
+    msg,mac = msgmac[:-32], msgmac[-32:]
+    # mac covers everything else: (IV+enc(data+padding))
+    if mac != hmac.new(hmacKey, msg, hashlib.sha256).digest():
+        raise ValueError("Corrupt encrypted data")
+    IV,ct = msg[:16], msg[16:]
+    print "dec", [tohex(IV), tohex(ct), tohex(mac)]
+    c = AES.new(aesKey, mode=AES.MODE_CBC, IV=IV)
+    padded_data = c.decrypt(ct)
+    print "padded data", tohex(padded_data)
+    pad = ord(padded_data[-1])
+    data = padded_data[:-pad]
+    return data
+
+
 
 if __name__ == '__main__':
     def one():
         email = u"foo@example.org"
         password = u"password"
-        authKey, cryptKey = do(email, password)
+        authKey, cryptKey = do_kdf(email, password)
         print "1 authKey :", tohex(authKey)
         print "1 cryptKey:", tohex(cryptKey)
-    one()
+    #one()
 
     def two():
         email = u"andré@example.org"
         password = u"pässwörd"
-        authKey, cryptKey = do(email, password)
-        print "2 authKey :", tohex(authKey)
-        print "2 cryptKey:", tohex(cryptKey)
+        data = '{"kéy": "valuë2"}'
+        m = encrypt(email, password, data)
     two()
-
