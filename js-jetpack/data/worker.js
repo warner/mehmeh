2fe731f741185e97b8db27c5681e4db1f6524be1
Brian Warner <warner@lothar.com>
2013-01-22 16:58:09 -0800

    got KDF working in page-worker <script>. Is slow.

--- js-jetpack/data/worker.js
+++ js-jetpack/data/worker.js
@@ -1,15 +1,16 @@
 
-console.log("worker.js loading");
+/* console.log is not available here */
+//console.log("worker.js loading");
 
 sjcl.random.addEntropy("seed", 8*32, "fake");
 
 
 addon.port.on("kdf", function(m) {
-    console.log("worker do kdf", m);
+    //console.log("worker do kdf", m);
     var keys = gombot_kdf(m.email, m.password);
-    console.log(" worker finish do kdf emit");
-    addon.port.emit("kdf-output", {keys: keys});
-    console.log(" worker finish do kdf");
+    //console.log(" worker finish do kdf emit");
+    addon.port.emit("kdf-done", {keys: keys});
+    //console.log(" worker finish do kdf");
 });
 
-console.log("worker.js loaded");
+//console.log("worker.js loaded");
--- js-jetpack/lib/main.js
+++ js-jetpack/lib/main.js
@@ -6,7 +6,7 @@ var data = require("self").data;
 
 console.log("creating page-worker");
 var worker = require("page-worker").Page({
-    contentUrl: data.url("worker.html")
+    contentURL: data.url("worker.html")
     // that uses <script> tags to pull in SJCL and the gombot KDF code. We
     // use worker.port.emit(type,data) to send requests, and get back
     // responses with worker.on(type, f(data)) . The worker code uses
@@ -15,6 +15,7 @@ var worker = require("page-worker").Page({
 });
 console.log("page-worker created");
 
+
 require("widget").Widget({
     id: "gombot-test-widget",
     label: "Gombot Test",
@@ -22,13 +23,13 @@ require("widget").Widget({
     onClick: function() {
         console.log("asking worker to kdf");
         var start = new Date().getTime();
-        worker.port.emit("kdf", { email: "andré@example.org",
-                                  password: "pässwörd" });
-        worker.on("kdf-done", function(m) {
+        worker.port.on("kdf-done", function(m) {
             var end = new Date().getTime();
             console.log("elapsed", (end-start)/1000);
             console.log("keys", JSON.stringify(m.keys));
         });
+        worker.port.emit("kdf", { email: "andré@example.org",
+                                  password: "pässwörd" });
         console.log("asked worker to kdf");
     }
 });
