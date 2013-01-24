7275bd7c89382a9b0825f45c0131e50f311f4b1d
Brian Warner <warner@lothar.com>
2013-01-24 13:14:48 -0800

    consolidate to use only one WebWorker

--- js-jetpack/data/gombot-worker1.js
+++ js-jetpack/data/gombot-worker1.js
@@ -14,37 +14,38 @@ console = {log: function(m, m2) {dump(tostr(m));
                                 }};
 //console.log("gombot-worker1.js loading");
 
+var worker = new Worker("gombot-worker2.js");
+
 addon.port.on("kdf", function(m) {
     //console.log("worker do test-webworker", m);
-    var w = new Worker("gombot-worker2.js");
-    w.onmessage = function(r) {
+    worker.onmessage = function(r) {
         //console.log(" worker finish test-webworker", r.data);
         var data = JSON.parse(r.data);
         addon.port.emit("kdf-done", {keys: data.keys,
                                      elapsed: data.elapsed});
         //console.log(" worker finish test-webworker sent response");
     };
-    w.postMessage({type: "kdf", email: m.email, password: m.password});
+    worker.postMessage({type: "kdf", email: m.email, password: m.password});
 });
 
 addon.port.on("encrypt", function(m) {
     var w = new Worker("gombot-worker2.js");
-    w.onmessage = function(r) {
+    worker.onmessage = function(r) {
         var data = JSON.parse(r.data);
         addon.port.emit("encrypt-done", {msgmac_b64: data.msgmac_b64,
                                          elapsed: data.elapsed});
     };
-    w.postMessage({type: "encrypt",
-                   keys: m.keys, data: m.data, forceIV: m.forceIV});
+    worker.postMessage({type: "encrypt",
+                        keys: m.keys, data: m.data, forceIV: m.forceIV});
 });
 
 addon.port.on("decrypt", function(m) {
     var w = new Worker("gombot-worker2.js");
-    w.onmessage = function(r) {
+    worker.onmessage = function(r) {
         var data = JSON.parse(r.data);
         addon.port.emit("decrypt-done", {plaintext: data.plaintext,
                                          elapsed: data.elapsed});
     };
-    w.postMessage({type: "decrypt",
-                   keys: m.keys, msgmac_b64: m.msgmac_b64});
+    worker.postMessage({type: "decrypt",
+                        keys: m.keys, msgmac_b64: m.msgmac_b64});
 });
